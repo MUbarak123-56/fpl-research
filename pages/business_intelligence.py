598d1bcf1ad93a66225f7a3ff83b8ec2d2f1274b
Mubarak Ganiyu <mubarak.a.ganiyu@vanderbilt.edu>
2024-02-10 17:59:01 -0600

    Update business_intelligence.py

--- pages/business_intelligence.py
+++ pages/business_intelligence.py
@@ -1,1 +1,10 @@
+import streamlit.components.v1 as components
 import streamlit as st
+
+st.set_page_config(layout='wide')
+st.header("FPL Business Intelligence Report")
+#st.set_page_config(theme="light")
+HtmlFile = open("qiskit-exam-prep-light-mode.html", 'r', encoding='utf-8')
+source_code = HtmlFile.read() 
+print(source_code)
+components.html(source_code, width = 1400, height = 900)
