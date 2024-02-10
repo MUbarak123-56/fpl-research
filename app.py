dfcba43bccc98559595b0ef24e35f7c09c34b9a7
Mubarak Ganiyu <mubarak.a.ganiyu@vanderbilt.edu>
2024-02-10 16:21:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -16,19 +16,18 @@ def data_load():
     df = pd.read_excel("data/fpl_data/full_df.xlsx")
     return df
 
-def run():
-    st.set_page_config(layout='wide', page_title = "Soccer Maestros in England")
-    
-    st.markdown("<h1 style='text-align: center; color: white;'>Soccer Maestros in England</h1>", unsafe_allow_html=True)
-    st.markdown("<h5 style='text-align: center; color: white;'>Authors: Mubarak Ganiyu, Nitipon 'Tony' Trimaitreepituk</h5>", unsafe_allow_html=True)
-    st.header("Introduction")
-    st.markdown("<h5 style='text-align: left; color: white;'>Our project analyzes data from the English Premier League Soccer. We used both actual teams results and"
+
+st.set_page_config(layout='wide', page_title = "Soccer Maestros in England")
+st.markdown("<h1 style='text-align: center; color: white;'>Soccer Maestros in England</h1>", unsafe_allow_html=True)
+st.markdown("<h5 style='text-align: center; color: white;'>Authors: Mubarak Ganiyu, Nitipon 'Tony' Trimaitreepituk</h5>", unsafe_allow_html=True)
+st.header("Introduction")
+st.markdown("<h5 style='text-align: left; color: white;'>Our project analyzes data from the English Premier League Soccer. We used both actual teams results and"
     + "the data from the fantasy premier league app(FPL).</h5>", unsafe_allow_html=True)
-    st.markdown("<h5 style='text-align: left; color: white;'>We analyzed team results to make prediction on the outcome of future games. We used the players' scores"
+st.markdown("<h5 style='text-align: left; color: white;'>We analyzed team results to make prediction on the outcome of future games. We used the players' scores"
     " from the fantasy premier league app to visualize the ability of each player from the start to the end of each season (2021-2024).</h5>", unsafe_allow_html=True)
-    new_data = data_load()
-    st.dataframe(new_data.head())
+new_data = data_load()
+st.dataframe(new_data.head())
     
 
-if __name__ == '__main__':
-    run()
+#if __name__ == '__main__':
+#    run()
