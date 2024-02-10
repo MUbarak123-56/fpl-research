39fe219ad943eb7e66b2a813402106007ef45b21
Mubarak Ganiyu <mubarak.a.ganiyu@vanderbilt.edu>
2024-02-10 17:41:25 -0600

    Update fpl_analyst.py

--- pages/fpl_analyst.py
+++ pages/fpl_analyst.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 #st.page_link("https://chat.openai.com/g/g-OxrmR8wbw-fantasy-league-analyst", label="FPL Analyst", icon="⚽")
-st.link_button("Have a chat with FPL Analyst", "hhttps://chat.openai.com/g/g-OxrmR8wbw-fantasy-league-analyst")
+st.link_button("Have a chat with FPL Analyst", "https://chat.openai.com/g/g-OxrmR8wbw-fantasy-league-analyst")
