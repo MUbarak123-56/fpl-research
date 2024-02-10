c4dfb58d4dfe76b13b582d020735ba212a27d596
Mubarak Ganiyu <mubarak.a.ganiyu@vanderbilt.edu>
2024-02-10 15:08:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import streamlit.components.v1 as components
 import PIL
 import io
 import numpy as np
-#from pathlib import Path
-from fastai.vision.all import *
 #import pathlib
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set(rc={'axes.facecolor':(0,0,0,0), 'figure.facecolor':(0,0,0,0)})
