c779ee5b3ce8d1f068ac771b5be8b321e80f5897
Mubarak Ganiyu <mubarak.a.ganiyu@vanderbilt.edu>
2024-02-10 17:52:12 -0600

    Update fpl_analyst.py

--- pages/fpl_analyst.py
+++ pages/fpl_analyst.py
@@ -1,26 +1,26 @@
 import streamlit as st
-
-import streamlit as st
 import pandas as pd
 from langchain.chat_models import ChatOpenAI
 from langchain.agents import create_pandas_dataframe_agent
 from langchain.agents.agent_types import AgentType
 
 # Page title
-st.set_page_config(page_title='FPL Analyst')
-st.write("Welcome to the FPL analyst page
+st.set_page_config(page_title='FPL Data Analyst')
+#st.title('🦜🔗 Ask the Data App')
 
-# Load CSV file
-def load_csv(input_csv):
-  df = pd.read_csv(input_csv)
+def load_file(input_file):
+  if input_file.name[-4:] == ".csv":
+      df = pd.read_csv(input_file)
+  elif input_file.name[-5:] == ".xlsx":
+      df = pd.read_excel(input_file)
   with st.expander('See DataFrame'):
     st.write(df)
   return df
 
 # Generate LLM response
-def generate_response(csv_file, input_query):
-  llm = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.2, openai_api_key=openai_api_key)
-  df = load_csv(csv_file)
+def generate_response(input_file, input_query):
+  llm = ChatOpenAI(model_name='gpt-4', temperature=0.1, openai_api_key=openai_api_key)
+  df = load_file(input_file)
   # Create Pandas DataFrame Agent
   agent = create_pandas_dataframe_agent(llm, df, verbose=True, agent_type=AgentType.OPENAI_FUNCTIONS)
   # Perform Query using the Agent
@@ -28,11 +28,9 @@ def generate_response(csv_file, input_query):
   return st.success(response)
 
 # Input widgets
-uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
+uploaded_file = st.file_uploader('Upload a CSV or Excel file', type=['csv', 'xlsx'])
 question_list = [
-  'How many rows are there?',
-  'What is the range of values for MolWt with logS greater than 0?',
-  'How many rows have MolLogP value greater than 0.',
+  'Who had the highest number of points on GW 38 in the 2022/23 season?",
   'Other']
 query_text = st.selectbox('Select an example query:', question_list, disabled=not uploaded_file)
 openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
@@ -45,5 +43,5 @@ if not openai_api_key.startswith('sk-'):
 if openai_api_key.startswith('sk-') and (uploaded_file is not None):
   st.header('Output')
   generate_response(uploaded_file, query_text)
-#st.page_link("https://chat.openai.com/g/g-OxrmR8wbw-fantasy-league-analyst", label="FPL Analyst", icon="⚽")
-st.link_button("Have a chat with our custom-made FPL Analyst on GPT", "https://chat.openai.com/g/g-OxrmR8wbw-fantasy-league-analyst")
+  
+st.link_button("You can also have a chat with our custom-made FPL Analyst on GPT", "https://chat.openai.com/g/g-OxrmR8wbw-fantasy-league-analyst")
