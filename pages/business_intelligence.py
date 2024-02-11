571af3e9d6364fd5f285b5847b6bff43ec1a2e67
Mubarak Ganiyu <mubarak.a.ganiyu@vanderbilt.edu>
2024-02-10 19:40:12 -0600

    Update business_intelligence.py

--- pages/business_intelligence.py
+++ pages/business_intelligence.py
@@ -4,6 +4,22 @@ import streamlit as st
 st.set_page_config(layout='wide')
 st.markdown("<h2 style='text-align: center;'>FPL Business Intelligence Report</h2>", unsafe_allow_html=True)
 
+def add_bg(image_file):
+        with open(image_file, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read())
+        st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+            background-size: cover;}}
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+         )
+add_bg("../new_pattern.jpg") 
+
 #st.header("FPL Business Intelligence Report")
 #st.set_page_config(theme="light")
 HtmlFile = open("fpl.html", 'r', encoding='utf-8')
