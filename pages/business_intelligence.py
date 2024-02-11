b22ac1e5dcd0bc1b71b428409a3b76634c87810d
Mubarak Ganiyu <mubarak.a.ganiyu@vanderbilt.edu>
2024-02-10 18:37:27 -0600

    Update business_intelligence.py

--- pages/business_intelligence.py
+++ pages/business_intelligence.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.set_page_config(layout='wide')
 st.header("FPL Business Intelligence Report")
 #st.set_page_config(theme="light")
-HtmlFile = open("fpl_bi.html", 'r', encoding='utf-8')
+HtmlFile = open("fpl.html", 'r', encoding='utf-8')
 source_code = HtmlFile.read() 
 print(source_code)
 components.html(source_code, width = 1400, height = 900)
