24031f1247e840a3587ee8a8fe2547255df9c0d5
Mubarak Ganiyu <mubarak.a.ganiyu@vanderbilt.edu>
2024-02-11 10:04:21 -0600

    Update app.py

--- app.py
+++ app.py
@@ -18,13 +18,8 @@ st.sidebar.header("Home 🏠")
 
 from st_pages import Page, add_page_title, show_pages
 
-#show_pages(
- #   [
-  #      Page("app.py", "Home", "🏠"),
-  #      Page("pages/business_intelligence.py","Business Intelligence", "📰"),
-   #     Page("pages/fpl_analyst.py", "FPL Analyst", "🤖"),
-   # ]
-#)
+show_pages([Page("app.py", "Home", "🏠"),Page("pages/business_intelligence.py","Business Intelligence", "📰"),Page("pages/fpl_analyst.py", "FPL Analyst", "🤖"),])
+
 st.markdown(
     """
 <style>
