de20ffa8532495ccdbfb6377097ee85e21ac7a75
Mubarak Ganiyu <mubarak.a.ganiyu@vanderbilt.edu>
2024-02-11 08:05:01 -0600

    Update business_intelligence.py

--- pages/business_intelligence.py
+++ pages/business_intelligence.py
@@ -9,6 +9,7 @@ st.set_page_config(layout='wide', page_title = "FPL Business Intelligence Report
 #add_page_title(layout="wide")
 st.markdown("<h2 style='text-align: center;'>📰 FPL Business Intelligence Report</h2>", unsafe_allow_html=True)
 st.markdown("<h6 style='text-align: center; color: red;'>For a great viewing experience, please use dark mode to view this app.</h6>", unsafe_allow_html=True)
+st.markdown("<h6 style='text-align: center; color: red;'>Use the sidebar to navigate to other pages.</h6>", unsafe_allow_html=True)
 
 st.write("Welcome to the FPL Business Reporting page. Using the dashboard below, you will be able to see different ways within which Fantasy Premier League data has been explored. Be sure to scroll through the tabs to view different charts and graphs, and use the filters on each page to transform the charts to notice significant changes. Lastly, view in full screen for the ultimate viewing experience. The full screen button is at the bottom right of the embedded dashboard.")
 
