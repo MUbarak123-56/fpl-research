b3814ff1746a0fd72d1f1d9c399d157d2b82b7b5
Mubarak Ganiyu <mubarak.a.ganiyu@vanderbilt.edu>
2024-02-10 20:37:27 -0600

    Create streamlit_app_section.py

---
+++ streamlit_app_section.py
@@ -0,0 +1,17 @@
+from st_pages import Page, add_page_title, show_pages
+
+show_pages(
+   [
+        Page("app.py", "Home", "🏠"),
+        # Can use :<icon-name>: or the actual icon
+        Page("pages/business_intelligence.py", "Business Intelligence", "📰"),
+        # The pages appear in the order you pass them
+        Page("pages/fpl_analyst.py", "FPL GPT Analyst", "🤖"),
+    ]
+)
+
+add_page_title()
+
+from st_pages import show_pages_from_config
+
+show_pages_from_config()
