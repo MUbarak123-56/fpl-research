02d2363cbc41badf60cfe8e150e5e0ac6ecd4bf3
Mubarak Ganiyu <mubarak.a.ganiyu@vanderbilt.edu>
2024-02-11 07:33:01 -0600

    Update fpl_analyst.py

--- pages/fpl_analyst.py
+++ pages/fpl_analyst.py
@@ -2,9 +2,10 @@ import streamlit as st
 import pandas as pd
 import base64
 
-st.set_page_config(page_title='FPL Data Analyst')
+st.set_page_config(page_title='FPL Analyst')
 st.markdown("<h1 style='text-align: center;'>📈 🤖 Welcome to the FPL Analyst</h1>", unsafe_allow_html=True)
 st.markdown("<h6 style='text-align: center;'>Hey There, Soccer Friends. Click the link below to chat with our FPL Data Analyst.</h6>", unsafe_allow_html=True)
+st.write("Our chatbot was built using GPT Builder by ingesting FPL data for it to learn from while drawing insights from current trends in the Premier League and soccer world. It is designed to assist users with having the best FPL experience by recommending how they can craft their team. This apps focuses heavily on historical performance for recommendation. It is still in its beta testing phase and might not be fully accurate.")
 def add_bg(image_file):
         with open(image_file, "rb") as image_file:
             encoded_string = base64.b64encode(image_file.read())
@@ -21,4 +22,4 @@ def add_bg(image_file):
          )
         
 add_bg("new_pattern.jpg") 
-st.link_button("Click here to be redirected to a new page.", "https://chat.openai.com/g/g-OxrmR8wbw-fpl-analyst")
+st.link_button("Click here to interact with FPL Analyst.", "https://chat.openai.com/g/g-OxrmR8wbw-fpl-analyst")
